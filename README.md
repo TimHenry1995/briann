62e622e27ddd9887ca33feba10a7486592555023
TimHenry1995 <tim.dick@maastrichtuniversity.nl>
2026-01-30 10:19:48 +0100

    reschuffled files for readthedocs

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # BrIANN
-This is a package for developing brain inspired artificial neural networks.
+This is a package for developing brain inspired artificial neural networks. 
+
+Bla bla blub
--- setup.py
+++ setup.py
@@ -3,5 +3,8 @@
 from distutils.core import setup
 from setuptools import find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 if __name__ == "__main__":
     setup(packages=find_packages(where='src'))
